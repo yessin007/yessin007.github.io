f343852f7529820c6ee3cbd5072a2ba00b2e7097
Yessin Khlif <yessin.khlif@esprit.tn>
2023-09-24 00:11:36 +0100

    npm install

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import helmet from 'koa-helmet';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    middleware: [helmet.contentSecurityPolicy()],
-  },
+
 })
