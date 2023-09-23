cd3f116145b3451433f55efa7d0d0fb0cc2104f3
Yessin Khlif <yessin.khlif@esprit.tn>
2023-09-24 00:34:48 +0100

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-})
+  server: {
+    middleware: (defaultMiddleware) => {
+      const middleware = [...defaultMiddleware];
+      middleware.push((req, res, next) => {
+        res.setHeader('X-Content-Type-Options', 'nosniff');
+        next();
+      });
+      return middleware;
+    },
+  },
+});
