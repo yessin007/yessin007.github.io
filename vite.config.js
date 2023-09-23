a021d4308ed0f30190b9ebd29cd832d7b1ca2468
Yessin Khlif <yessin.khlif@esprit.tn>
2023-09-23 20:57:11 +0100

    server set up Content-type : nosniff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import helmet from 'koa-helmet';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middleware: [helmet.contentSecurityPolicy()],
+  },
 })
