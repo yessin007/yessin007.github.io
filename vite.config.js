1a0fb0da69336e762abec9d11a0b7471fcb36c96
Yessin Khlif <yessin.khlif@esprit.tn>
2023-09-24 21:10:28 +0200

    config vite testing

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    middleware: (defaultMiddleware) => {
-      const middleware = [...defaultMiddleware];
-      middleware.push((req, res, next) => {
-        res.setHeader('X-Content-Type-Options', 'nosniff');
-        next();
-      });
-      return middleware;
-    },
+    headers: {
+      'Content-Type-Options': 'nosniff'
+    }
   },
 });
